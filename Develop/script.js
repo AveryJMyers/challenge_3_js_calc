4d185d34ee747e9fd0a2d58698ff49217086df58
Avery Myers <averyjmyers@hotmail.com>
2023-05-22 17:32:13 -0700

    JS comments added

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,31 @@
 // Assignment code here
 
 
-
+//prompt to select how many characters in the passowrd
 while (true) {
-  var lengthInput = prompt("How many characters do you want your password to be? (Between 8-18)");
+  var lengthInput = prompt("How many characters do you want your password to be? (Between 8-128)");
   if (lengthInput >= 8 && lengthInput <= 128) {
       passwordLength = parseInt(lengthInput);
-      break; // Exit the loop if the input is valid
+      break; // exits this loop if the user meets the criteria
   } else {
       alert("Invalid password length. Please enter a number between 8 and 128.");
-      continue;
+      continue; // reruns this loop if the user fails to meet the criteria
     }
   }
 
-
+// user prompts to select what they want included in their password
 var includeUppercase = confirm("Select OK to include uppercase letters.");
 var includeLowercase = confirm("Select OK to include lowercase letters.");
 var includeNumbers = confirm("Select OK to include numbers.");
 var includeSpecialChars = confirm("Select OK to include special characters.");
 
+//booleans effected by the user responses
 includeUppercase = includeUppercase === true;
 includeLowercase = includeLowercase === true;
 includeNumbers = includeNumbers === true;
 includeSpecialChars = includeSpecialChars === true;
 
-
+//generates the password
 function generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSpecialChars){
   //define character sets for each option
   
@@ -33,9 +34,11 @@ function generatePassword(passwordLength, includeUppercase, includeLowercase, in
   var numberChars = "0123456789";
   var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
+  //creates an empty string and an empty array
   var newPassword = "";
   var charSets = [];
 
+  //checks the boolean, puses the characters if true.
   if(includeUppercase){
     charSets.push(uppercaseChars);
   }
@@ -49,12 +52,13 @@ function generatePassword(passwordLength, includeUppercase, includeLowercase, in
   if(includeSpecialChars)
     charSets.push(specialChars);
 
+  // creates an error if the user doesn't select any character types.
   if(charSets.length === 0){
     alert("Please select at least one character type.");
     return;
   }
 
-
+//selects random charSet, then chooses a random letter from the random charSet until the password length is complete. 
   for (var i = 0; i < passwordLength; i++){
       var randomCharSet=charSets[Math.floor(Math.random() * charSets.length)];
       randomChar=randomCharSet[Math.floor(Math.random() * randomCharSet.length)];
@@ -73,7 +77,7 @@ function generatePassword(passwordLength, includeUppercase, includeLowercase, in
 var generatedPassword = generatePassword(passwordLength, includeUppercase, includeLowercase, includeNumbers, includeSpecialChars)
 
 // Display the generated password in an alert
-alert("Your generated password is: " + generatedPassword);
+// alert("Your generated password is: " + generatedPassword);
 
 // Or update the textarea in the HTML with the generated password
 var passwordTextarea = document.getElementById("password");
